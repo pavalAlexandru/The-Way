13fa26a825ad7a4ac94d497a5062ac8075ea6c11
andu <alexandrugeorgepaval@gmail.com>
2026-06-09 18:16:49 +0300

    almost done

--- Backend/Backend/Repositories/AppDbContext.cs
+++ Backend/Backend/Repositories/AppDbContext.cs
@@ -21,7 +21,7 @@ public class AppDbContext : DbContext
             new Player { Id = 1, Username = "Andu" }
         );
         modelBuilder.Entity<GameConfig>().HasData(
-            new GameConfig { Id = 1, SafePath = "2,3,1,4", Obstacles = "1-1,4-2,2-3,3-4,4-4" }
+            new GameConfig { Id = 1, SafePath = "2,3,1,4", Obstacles = "1-1,4-2,2-3,3-4,1-4" }
         );
     }
 }
